8ec595043eacac970416b520da4d0396a02d5e84
Mahmoud Abdulrhman <mahmoudtheeb@gmail.com>
2020-08-07 02:22:54 -0700

    add password length

--- script.js
+++ script.js
@@ -1,5 +1,42 @@
 // Assignment code here
+const randomFunc = {
+	lower: getRandomLower,
+	upper: getRandomUpper,
+	number: getRandomNumber,
+	symbol: getRandomSymbol
+}
+//Generator functions
+function getRandomLower(){
+  return String.fromCharCode(Math.floor(Math.random()*26)+97);
+}
+function getRandomUpper(){
+  return String.fromCharCode(Math.floor(Math.random()*26)+65);
+}
+function getRandomNumber(){
+  return String.fromCharCode(Math.floor(Math.random()*10)+48);
+}
+function getRandomSymbol(){
+  const symbol = "!@#$%^&*(){}[]=<>/,.|~?";
+  return symbol[Math.floor(Math.random()*symbol.length)];
+}
 
+function passwordCrai(){
+var length = window.prompt(" How many characters would you like your password? Choose between 8 and 128. ");
+while (length < 8 || length > 128 ){
+  window.alert(" You need to have a valid answer. ")
+  return passwordCrai()
+}
+var lower =confirm(" Will this contain Lowercase letters? ");
+if ( lower === true){
+ getRandomLower()
+ console.log(getRandomLower());
+}
+
+
+};
+passwordCrai();
+
+const myPassword = window.prompt("my password");
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
