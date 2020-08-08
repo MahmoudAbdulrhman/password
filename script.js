192d21722b7dbe6771cdfdce64ed6df2d308c5a7
Mahmoud Abdulrhman <mahmoudtheeb@gmail.com>
2020-08-08 00:47:23 -0700

    add if statment

--- script.js
+++ script.js
@@ -1,12 +1,5 @@
 // Assignment code here
-var enter;
-var	lower;
-var	symbol;
-var number;
-var upper; 
-var space = [];
-var choices;
-
+document.querySelector("#generate").addEventListener("click", writePassword);
 //Generator functions
 function getRandomLower(){
   return String.fromCharCode(Math.floor(Math.random()*26)+97);
@@ -17,50 +10,81 @@ function getRandomUpper(){
 function getRandomNumber(){
   return String.fromCharCode(Math.floor(Math.random()*10)+48);
 }
-function getRandomSymbol(){
-  const symbol = "!@#$%^&*(){}[]=<>/,.|~?";
-  return symbol[Math.floor(Math.random()*symbol.enter)];
+// function getRandomSymbol(){
+  var symbol = ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?","~"];
+//   return symbol[Math.floor(Math.random()*symbol.enter)];
+// };
+
+// Variable Declaration 
+var confirmLength = "";
+var confirmSpecialCharacter;
+var confirmNumericCharacter;
+var confirmUpperCase;
+var confirmLowerCase;
+
+function generatePassword() {
+  var confirmLength = (prompt("How many characters would you like your password to contain?"));
+
+  // Loop if answer is outside the parameters 
+  while(confirmLength <= 8 || confirmLength >= 128) {
+      alert("Password length must be between 8-128 characters Try again");
+      var confirmLength = (prompt("How many characters would you like your password to contain?"));
+      } 
+
+      // Repeat back how many charactes the user will have  
+      alert(`Your password will have ${confirmLength} characters`);
+
+    // Determine parameters of password 
+    var confirmSpecialCharacter = confirm("Click OK to confirm if you would like to include special characters");
+    var confirmNumericCharacter = confirm("Click OK to confirm if you would like to include numeric characters");    
+    var confirmLowerCase = confirm("Click OK to confirm if you would like to include lowercase characters");
+    var confirmUpperCase = confirm("Click OK to confirm if you would like to include uppercase characters");
+      // Loop if answer is outside the parameters 
+      while(confirmUpperCase === false && confirmLowerCase === false && confirmSpecialCharacter === false && confirmNumericCharacter === false) {
+        alert("You must choose at least one parameter");
+        var confirmSpecialCharacter = confirm("Click OK to confirm if you would like to include special characters");
+        var confirmNumericCharacter = confirm("Click OK to confirm if you would like to include numeric characters");    
+        var confirmLowerCase = confirm("Click OK to confirm if you would like to include lowercase characters");
+        var confirmUpperCase = confirm("Click OK to confirm if you would like to include uppercase characters");   
+    } ;
+
+
+    // Assign an action to the password parameters NEED TO FIX THIS
+    var passwordCharacters = []
+      
+    if (confirmSpecialCharacter) {
+      passwordCharacters = passwordCharacters.concat(symbol)
+    }
+
+    if (confirmNumericCharacter) {
+      passwordCharacters = passwordCharacters.concat(getRandomNumber())
+    }
+      
+    if (confirmLowerCase) {
+      passwordCharacters = passwordCharacters.concat(getRandomLower())
+    }
+
+    if (confirmUpperCase) {
+      passwordCharacters = passwordCharacters.concat(getRandomUpper())
+    }
+
+      console.log(passwordCharacters);
+
+// Empty string to be filled based on for loop selecting random characters from the array
+var randomPassword = ""
+      
+for (var i = 0; i < confirmLength;i++) {
+  randomPassword = randomPassword + passwordCharacters[Math.floor(Math.random() * passwordCharacters.length)];
+  console.log(randomPassword)
 }
-
-
-
-
-
-
-
-
-
-
-
-function generatePassword(){
-var enter = parseInt(prompt(" How many characters would you like your password? Choose between 8 and 128. "));
-if (!enter) {
-  alert(" This need a value")
-   } else if  (enter < 8 || enter > 128 || enter == null ){
-  enter = parseInt(prompt(" You must choose a # between 8 and 128. "))
-  return generatePassword()
-} else {
-  upper = confirm(" You want it to contain a UpperCase letters? ");
-  lower = confirm(" You want it to lowerCase letters? ");
-  number = confirm(" You want it to Number letters? ");
-  symbol = confirm(" You want it to Symbol letters? ");
-  
-}
+return randomPassword;
 };
-
-
-    
-
-
    
 
 // Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+ var generateBtn = document.querySelector("#generate");
 
-generateBtn.addEventListener("click", function () {
-  ps = generatePassword();
-  document.getElementById("password").placeholder = ps;
-});
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
